7cfcb26b8e602228d6b5ca45c1d48b4a73885ba5
Kolade <koladeadewuyi@gmail.com>
2018-06-04 14:47:23 +0100

    Move package model

---
+++ src/main/scala/com/kolade/offers/model/package.scala
@@ -0,0 +1,24 @@
+package com.kolade.offers
+
+import akka.http.scaladsl.server.Rejection
+import org.joda.money.Money
+import org.joda.time.DateTime
+
+package object model {
+
+  object Expired extends Enumeration {
+    type Expired = Value
+    val Yes, No = Value
+  }
+
+  final case class Price(cost: Money)
+
+  final case class Validity(startDate: DateTime, endDate: DateTime)
+
+  final case class Offer(offerId: String, description: String, price: Price, validity: Validity, expired: Option[Expired.Value] = None)
+
+  final case class FieldErrorInfo(name: String, error: String)
+
+  final case class ValidationRejection(invalidFields: Seq[FieldErrorInfo]) extends Rejection
+
+}
